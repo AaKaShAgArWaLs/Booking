473e451f2ac38364ac5ccfea15e338a4b700d321
Aakash <aakashrkl603@gmail.com>
2025-08-31 14:42:58 +0530

    api

--- services/bookingApi.js
+++ services/bookingApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000'; // Change to your backend URL
+const API_BASE_URL = 'https://booking-seven-wheat.vercel.app'; // Change to your backend URL
 
 class BookingAPI {
   constructor() {
